ced9c9811e278047d962dc068ee15c59643ac31c
Marko Anastasov <marko@renderedtext.com>
2013-10-10 10:44:36 +0200

    Dummy reformat of config.rb

--- config.rb
+++ config.rb
@@ -1,6 +1,8 @@
 # the directory we're running from
 base_dir Dir.pwd + "/"
-# # where to store cached files and cookbook output
+
+# where to store cached files and cookbook output
 file_cache_path base_dir + "cache/"
-# # where to find cookbooks
+
+# where to find cookbooks
 cookbook_path base_dir + "cookbooks/"
